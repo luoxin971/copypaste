cf19cc765cbdf738c66c63ff496f35d9cb6bb9e0
xin <luoxin9712@163.com>
2021-09-22 21:06:47 +0800

    add sql file and delete test

--- src/test/java/com/xin/copypaste/CopypasteApplicationTests.java
+++ src/test/java/com/xin/copypaste/CopypasteApplicationTests.java
@@ -11,30 +11,5 @@ import java.util.List;
 @SpringBootTest
 class CopypasteApplicationTests {
 
-    @Autowired
-    ContentMapper contentMapper;
-
-    @Test
-    void contextLoads() {
-        Content content = new Content();
-        content.setBrowser("safari");
-        content.setText("test second text");
-        content.setIp("192.168.1.214");
-        contentMapper.insertContent(content);
-    }
-
-    @Test
-    void test1() {
-        Content c = contentMapper.findById(2);
-        System.out.println(c);
-    }
-
-    @Test
-    void test2() {
-        List<Content> l = contentMapper.findAllContent();
-        for (Content content : l) {
-            System.out.println(content);
-        }
-    }
 
 }
