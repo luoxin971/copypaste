91602a41447f0b491a28bced97aaf5b845ab1b9b
xin <luoxin9712@163.com>
2021-09-23 18:00:16 +0800

    add redis

---
+++ src/main/java/com/xin/copypaste/config/LettuceRedisConfig.java
@@ -0,0 +1,29 @@
+package com.xin.copypaste.config;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+public class LettuceRedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lcf) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        redisTemplate.setConnectionFactory(lcf);
+        return redisTemplate;
+    }
+}
--- src/test/java/com/xin/copypaste/CopypasteApplicationTests.java
+++ src/test/java/com/xin/copypaste/CopypasteApplicationTests.java
@@ -1,15 +1,119 @@
 package com.xin.copypaste;
 
-import com.xin.copypaste.entity.Content;
-import com.xin.copypaste.mapper.ContentMapper;
+import com.xin.copypaste.util.RedisUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class CopypasteApplicationTests {
 
+    @Autowired
+    RedisTemplate<String, String> stringRedisTemplate;
 
+    @Autowired
+    RedisTemplate<String, Object> objectRedisTemplate;
+
+    @Autowired
+    RedisUtils redisUtils;
+
+    @Test
+    public void testString() {
+        stringRedisTemplate.opsForValue().set("strKey", "a");
+        System.out.println(stringRedisTemplate.opsForValue().get("strKey"));
+    }
+
+    @Test
+    public void testObject() {
+        Computer computer = new Computer();
+        computer.setId(2);
+        computer.setName("Mac");
+        objectRedisTemplate.opsForValue().set("apple", computer);
+        System.out.println(objectRedisTemplate.opsForValue().get("apple"));
+    }
+
+    @Test
+    public void test1() {
+        Collection<String> res = t("a", "b", "c");
+        for (String re : res) {
+            System.out.println(re);
+        }
+    }
+
+    private Collection t(String... arg) {
+        return Arrays.stream(arg).collect(Collectors.toList());
+    }
+
+    @Test
+    public void testExpire() {
+        redisUtils.set("dinner", "rice");
+        redisUtils.expire("dinner", 60);
+        System.out.println(redisUtils.getExpire("dinner"));
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(redisUtils.get("dinner"));
+    }
+
+    @Test
+    public void test3() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("apple", new Computer(6, "max"));
+        map.put("huawei", new Computer(7, "matebook"));
+        map.put("lenvon", new Computer(8, "legion"));
+        redisUtils.hmset("computer", map);
+        System.out.println(redisUtils.hmget("computer"));
+        System.out.println(redisUtils.hget("computer", "lenvon"));
+    }
+
+
+}
+class Computer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public Computer() {
+    }
+
+    public Computer(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
